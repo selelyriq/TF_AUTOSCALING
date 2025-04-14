8a0972793d81600c2e4a033fb58d73dfc394e269
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-14 17:56:11 -0400

    fmt

--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "launch_template_name" {
-  type = string
+  type        = string
   description = "The name of the launch template"
 }
 
 variable "autoscaling_group_name" {
-  type = string
+  type        = string
   description = "The name of the autoscaling group"
 }
 
 variable "lb_name" {
-  type = string
+  type        = string
   description = "The name of the load balancer"
 }
 
 variable "security_group_name" {
-  type = string
+  type        = string
   description = "The name of the security group"
 }
 
 variable "target_group_name" {
-  type = string
+  type        = string
   description = "The name of the target group"
 }
 
 variable "web_app_ami" {
-  type = string
+  type        = string
   description = "The AMI ID for the web app"
 }
 
 variable "web_app_instance_type" {
-  type = string
+  type        = string
   description = "The instance type for the web app"
 }
 
 variable "web_app_min_size" {
-  type = number
+  type        = number
   description = "The minimum number of instances for the web app"
 }
 
 variable "web_app_max_size" {
-  type = number
+  type        = number
   description = "The maximum number of instances for the web app"
 }
